5b32e52bf9b4e5c141ea04ba5b522d51b4230f09
zenkkor <zenkkor@gmail.com>
2017-10-18 14:23:26 +0200

    Remove unwanted build files

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
+
+import * as Payment from 'payment';
 export * from './ngxcreditcard.module';
 export * from './validators/CreditCard.validator';
--- ngxcreditcard.module.ts
+++ ngxcreditcard.module.ts
@@ -4,6 +4,8 @@ import { CCNumberFormatDirective } from './directives/cc-number-format.directive
 import { CCExpiryFormatDirective } from './directives/cc-expiry-format.directive';
 import { CCCvcFormatDirective } from './directives/cc-cvc-format.directive';
 
+import * as Payment from 'payment';
+
 const CC_DIRECTIVES = [
 	CCNumberFormatDirective,
 	CCExpiryFormatDirective, 
